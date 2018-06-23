6f52cbbbbe07fdeae9fc77c5694bb9f2237e5fb9
bbielsa <bgb7@njit.edu>
2018-06-23 17:29:29 -0400

    Call counterSpam on a spammer when spam is detected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const DiscordClient = require('discord.js').Client
 const config = require('config')
 const SpamDetector = require('./spamdetector').SpamDetector
+const counterSpam = require('./counterspam').counterSpam
 
 
 const clientSecret = config.get('token')
 const client = new DiscordClient()
 const spamDetector = new SpamDetector(client)
+
+spamDetector.on('spamDetected', spam => {
+    const spammer = spam.author
+    const spamChannel = spam.channel
+
+    counterSpam(client, spammer, spamChannel)
+})
 
 client.login(clientSecret)
