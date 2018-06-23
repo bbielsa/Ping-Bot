db531852f682ff86c5b65e5a5a036ec18cac775f
bbielsa <bgb7@njit.edu>
2018-06-23 17:27:58 -0400

    Lazy create the rate limiters for spammers

--- src/spamdetector.js
+++ src/spamdetector.js
@@ -1,14 +1,30 @@
 const RateLimiter = require('limiter').RateLimiter
+const EventEmitter = require('events')
 
-
-class SpamDetector {
+class SpamDetector extends EventEmitter {
 
     constructor(discordClient) {
+        super()
+
         this.discordClient = discordClient
         this.limiters = []
 
         this.discordClient.on('message', message => this.onMessage(message))
     }
+
+    rateLimiterFor(user) {
+        const id = user.id
+
+        if(id in this.limiters) {
+            return this.limiters[id]
+        }
+        else {
+            const limiter = new RateLimiter(3, 'minute', true)
+            this.limiters[id] = limiter
+
+            return limiter
+        }      
+     }
 
     onMessage(message) {
         const hasMentions = message.mentions.users.size > 0
@@ -19,8 +35,13 @@ class SpamDetector {
 
         const author = message.author
         const id = author.id
+        const channel = message.channel
+        const limiter = this.rateLimiterFor(author)
 
-        this.limiters[id] = new RateLimiter(5, 'minute')
+        limiter.removeTokens(1, (error, remaining) => {
+            if(remaining < 0)
+                this.emit('spamDetected', {author, channel})
+        })
     }
 
 }
